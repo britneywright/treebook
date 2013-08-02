f73426b829fc9ea0f5e17d3f1debe04b2dba7d28
Britney Wright <blw06g@gmail.com>
2013-08-02 15:18:01 -0400

    Updated tests

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -11,37 +11,42 @@ class StatusesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:statuses)
   end
 
-  test "should get new" do
+  test "should be redirected when not logged in" do
+    get :new
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should render the new page when logged in" do
+    sign_in users(:britney)
     get :new
     assert_response :success
-  end
+   end
 
-  test "should create status" do
-    assert_difference('Status.count') do
-      post :create, status: { content: @status.content, name: @status.name }
-    end
-
-    assert_redirected_to status_path(assigns(:status))
-  end
+  test "should be logged in to post a status" do
+    post :create, status: { content: "Hello"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end  
 
   test "should create status when logged in" do
     sign_in users(:britney)
 
     assert_difference('Status.count') do
-      post :create, status { content: @status.content }
+      post :create, status: { content: @status.content}
     end
 
-    assert_redirect_to status_path(assigns(:status))
+    assert_redirected_to status_path(assigns(:status))
 end
 
   test "should create status for the current user when logged in" do
     sign_in users(:britney)
 
     assert_difference('Status.count') do
-      post :create, status { content: @status.content }
+      post :create, status: { content: @status.content }
     end
 
-  assert_redirect_to status_path(assigns(:status))
+  assert_redirected_to status_path(assigns(:status))
   assert_equal assigns(:status).user_id, users(:britney).id
 end
 
@@ -56,8 +61,20 @@ end
   end
 
   test "should update status" do
-    put :update, id: @status, status: { content: @status.content, name: @status.name }
+    put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should get edit when logged in" do
+    sign_in users(:britney)
+    get :edit, id: @status
+    assert_response :success
+  end
+
+  test "should redirect status update when not logged in" do
+    put :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
   end
 
   test "should update status when logged in" do
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -21,16 +21,25 @@ class UserTest < ActiveSupport::TestCase
    
    test "a user should have a unique profile name" do
     user = User.new
-    user.profile_name = users(:britneyw).profile_name
+    user.profile_name = users(:britney).profile_name
     assert !user.save
     assert !user.errors[:profile_name].empty? 
    end
 
    test "a user should have a profile name without spaces" do
-    user = User.new
+    user = User.new(first_name: 'Britney', last_name: 'Wright', email: 'britney@britneywright.com')
+    user.password = user.password_confirmation = 'Blondie8'
     user.profile_name = "My Profile With Spaces"
+    
     assert !user.save
     assert !user.errors[:profile_name].empty?
     assert user.errors[:profile_name].include?("Must be formatted correctly.")
    end
+
+   test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Britney', last_name: 'Wright', email: 'britney@britneywright.com')
+    user.password = user.password_confirmation = 'Blondie8'
+    user.profile_name = 'britneyw'
+    assert user.valid?
+   end 
 end
